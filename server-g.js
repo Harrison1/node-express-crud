e8abaab4faf3241e3a3ab6920ea46e369208c984
Harrison <hmcguire13@gmail.com>
2016-09-25 21:00:30 -0700

    working crud app

--- server-g.js
+++ server-g.js
@@ -52,6 +52,55 @@ router.route('/superheros/:url').get((req, res) => {
 	Superhero.findOne(query, (err, data) => {
 		(err) ? res.send(err) : res.render('./app/views/profile.html', {superhero: data});
 	});	
+}).post((req, res) => {
+	const query = {"url": req.params.url.toLowerCase()};
+	const superhero = req.body;
+	superhero.url = req.body.name.toLowerCase();
+	Superhero.findOneAndUpdate(query, superhero, { new: true }, function (err, superhero) {
+	  if (err) {return res.send(err)};
+	  // res.send(superhero);
+	  // superhero.url = req.body.name.toLowerCase();
+	  res.redirect('/superheros/' + superhero.url);
+	});
+	// Superhero.update({url: req.params.url.toLowerCase()}, req.body, function(err, superhero){
+
+	// 	superhero.url = req.body.name.toLowerCase();
+
+	// 	res.redirect('/superheros/' + superhero.url);
+	// })
+	// const query = {"url": req.params.url.toLowerCase()};
+	// 	Superhero.findOne(query, (err, superhero) => {
+	// 	if (err) {
+	// 		res.send(err);
+	// 	}
+
+	// 	superhero.power = req.body.power;
+		
+	// 	superhero.save((err) => {
+	// 		if (err) {
+	// 			console.log("update error");
+	// 			res.send(err)
+	// 		}
+
+	// 		console.log("update sucess");
+	// 		res.redirect('/superheros/' + superhero.url);
+	// 	});
+	// });	
+});
+
+router.route('/superheros/:url/edit').get((req, res) => {
+	swig.invalidateCache();
+	const query = {"url": req.params.url.toLowerCase()};
+	Superhero.findOne(query, (err, data) => {
+		(err) ? res.send(err) : res.render('./app/views/edit.html', {superhero: data});
+	});	
+});
+
+router.route('/superheros/:url/delete').get((req, res) => {
+	const query = {"url": req.params.url.toLowerCase()};
+	Superhero.findOneAndRemove(query, (err, data) => {
+		(err) ? res.send(err) : res.redirect('/');
+	});	
 });
 
 
